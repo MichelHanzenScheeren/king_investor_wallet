f119a62193ef852a16c08ef9437e50e973558bd1
Michel Scheeren <michelhanzenscheeren@gmail.com>
2023-05-23 09:29:00 -0300

    increase: turn Quote attributes private and create getters

--- lib/src/domain/entities/quote.dart
+++ lib/src/domain/entities/quote.dart
@@ -4,41 +4,56 @@ import 'package:king_investor_wallet/src/domain/value_objects/positive_number_vo
 import 'package:result_dart/result_dart.dart';
 
 class Quote extends Entity {
-  final PositiveNumberVO price;
-  final PositiveNumberVO high;
-  final PositiveNumberVO low;
-  final PositiveNumberVO yearHigh;
-  final PositiveNumberVO yearLow;
-  final NumberVO changePercentage;
-  final NumberVO changePercentageMonth;
-  final NumberVO changePercentageYear;
+  final PositiveNumberVO _price;
+  final PositiveNumberVO _high;
+  final PositiveNumberVO _low;
+  final PositiveNumberVO _yearHigh;
+  final PositiveNumberVO _yearLow;
+  final NumberVO _changePercentage;
+  final NumberVO _changePercentageMonth;
+  final NumberVO _changePercentageYear;
 
   Quote({
     super.id,
-    required this.price,
-    required this.high,
-    required this.low,
-    required this.yearHigh,
-    required this.yearLow,
-    required this.changePercentage,
-    required this.changePercentageMonth,
-    required this.changePercentageYear,
-  });
+    required PositiveNumberVO price,
+    required PositiveNumberVO high,
+    required PositiveNumberVO low,
+    required PositiveNumberVO yearHigh,
+    required PositiveNumberVO yearLow,
+    required NumberVO changePercentage,
+    required NumberVO changePercentageMonth,
+    required NumberVO changePercentageYear,
+  })  : _price = price,
+        _high = high,
+        _low = low,
+        _yearHigh = yearHigh,
+        _yearLow = yearLow,
+        _changePercentage = changePercentage,
+        _changePercentageMonth = changePercentageMonth,
+        _changePercentageYear = changePercentageYear;
 
-  bool get hasValidPrice => price.isValid;
+  double get price => _price.value;
+  double get high => _high.value;
+  double get low => _low.value;
+  double get yearHigh => _yearHigh.value;
+  double get yearLow => _yearLow.value;
+  double get changePercentage => _changePercentage.value;
+  double get changePercentageMonth => _changePercentageMonth.value;
+  double get changePercentageYear => _changePercentageYear.value;
+  bool get hasValidPrice => _price.isValid;
 
   @override
   Result<Quote, String> validate() {
     return super
         .validate()
-        .flatMap((success) => price.validate().pure(this))
-        .flatMap((success) => high.validate().pure(this))
-        .flatMap((success) => low.validate().pure(this))
-        .flatMap((success) => yearHigh.validate().pure(this))
-        .flatMap((success) => yearLow.validate().pure(this))
-        .flatMap((success) => changePercentage.validate().pure(this))
-        .flatMap((success) => changePercentageMonth.validate().pure(this))
-        .flatMap((success) => changePercentageYear.validate().pure(this))
+        .flatMap((success) => _price.validate().pure(this))
+        .flatMap((success) => _high.validate().pure(this))
+        .flatMap((success) => _low.validate().pure(this))
+        .flatMap((success) => _yearHigh.validate().pure(this))
+        .flatMap((success) => _yearLow.validate().pure(this))
+        .flatMap((success) => _changePercentage.validate().pure(this))
+        .flatMap((success) => _changePercentageMonth.validate().pure(this))
+        .flatMap((success) => _changePercentageYear.validate().pure(this))
         .pure(this);
   }
 }
